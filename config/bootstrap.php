60b48f4762f6759d324f0d1ea7238bf432d7dd33
makecir <konpeist@yahoo.co.jp>
2020-07-04 07:49:49 +0900

    Fix: DBに接続できるように修正

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -105,6 +105,7 @@ if (Configure::read('debug')) {
  * Check http://php.net/manual/en/timezones.php for list of valid timezone strings.
  */
 date_default_timezone_set(Configure::read('App.defaultTimezone'));
+//date_default_timezone_set('Asia/Tokyo');
 
 /*
  * Configure the mbstring extension to use the correct encoding.
