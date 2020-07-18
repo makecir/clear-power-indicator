b461f3ae70b700a3a2d7f0cc126de2071d564ca8
makecir <konpeist@yahoo.co.jp>
2020-07-19 05:14:45 +0900

    Fix: ローカライズを加筆

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -1,7 +1,7 @@
 <!-- in /templates/Users/login.php -->
 <div class="users form">
     <?= $this->Flash->render() ?>
-    <h3>Login</h3>
+    <h3><?= __('Login') ?></h3>
     <?= $this->Form->create() ?>
     <fieldset>
         <legend><?= __('Please enter your username and password') ?></legend>
@@ -11,5 +11,5 @@
     <?= $this->Form->submit(__('Login')); ?>
     <?= $this->Form->end() ?>
 
-    <?= $this->Html->link("Add User", ['action' => 'add']) ?>
+    <?= $this->Html->link(__('Register'), ['action' => 'add']) ?>
 </div>
