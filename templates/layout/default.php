a1c71da7875d10ecd745712a7ab8d59b110fed15
makecir <konpeist@yahoo.co.jp>
2020-07-21 18:51:28 +0900

    Add: about.phpの雛型を追加

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -32,6 +32,7 @@ $cakeDescription = 'CPI : Clear Power Indicator ';
 
     <!-- <?= $this->Html->css('milligram.min.css') ?>
     <?= $this->Html->css('cake.css') ?> -->
+    <!-- using bootstrap css -->
     <?= $this->Html->css('bootstrap.min.css') ?>
 
     <?= $this->fetch('meta') ?>
@@ -61,6 +62,13 @@ $cakeDescription = 'CPI : Clear Power Indicator ';
                     <?= $this->Html->link(
                         __('User'),
                         ['controller' => 'Users', 'action' => 'index'],
+                        ['class' => 'nav-link'])
+                    ?>
+                </li>
+                <li class="nav-item">
+                    <?= $this->Html->link(
+                        __('About'),
+                        ['controller' => 'Pages', 'action' => 'about'],
                         ['class' => 'nav-link'])
                     ?>
                 </li>
@@ -110,6 +118,7 @@ $cakeDescription = 'CPI : Clear Power Indicator ';
     <footer>
     </footer>
 
+    <!-- using bootstrap js -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
