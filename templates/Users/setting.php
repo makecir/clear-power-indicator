874cbbadffb6e19e3554998fff8fe1cd31cf415f
makecir <konpeist@yahoo.co.jp>
2020-08-01 22:22:48 +0900

    Add: 連絡先変更フォームを追加

--- templates/Users/setting.php
+++ templates/Users/setting.php
@@ -7,16 +7,29 @@
             <div class="password change form content">
                 <?= $this->Form->create($user) ?>
                 <fieldset>
-                    <h4 class="card-title"><?= __('公開設定') ?></h4>
-                    <p>
-                        パブリック：情報の公開と被フォローを特に制限しない</br>
-                        プライベート：一部の情報はフォローワーのみ閲覧可能・被フォロー時にフォローフレーズを要求する</br>
-                        (鍵アカウントをフォローするには、本人から合言葉を聞く必要があるというイメージ)</br>
-                    </p>
-                    <?php
-                        echo $this->Form->select('private_level',$user->private_dict);
-                        echo $this->Form->control('follow_pass',['type' => 'text']);
-                    ?>
+                    <div class="mb-3">
+                        <h4 class="card-title"><?= __('公開設定') ?></h4>
+                        <p>
+                            パブリック：情報の公開と被フォローを特に制限しない</br>
+                            プライベート：一部の情報はフォローワーのみ閲覧可能・被フォロー時にフォローフレーズを要求する</br>
+                            (鍵アカウントをフォローするには、本人から合言葉を聞く必要があるというイメージ)</br>
+                        </p>
+                        <?php
+                            echo $this->Form->select('private_level',$user->private_dict);
+                            echo $this->Form->control('follow_pass',['type' => 'text']);
+                        ?>
+                    </div>
+                    <div class="mb-3">
+                        <h4 class="card-title"><?= __('連絡先を設定') ?></h4>
+                        <p>
+                            ユーザーIDまたはパスワードを忘れ、ログインが不可能になった場合、下記の情報を登録しておくことで、当サイトの運営と連絡を取ることが可能です。</br>
+                            メールアドレス情報は第三者に公開されず、上記の連絡においての本人確認以外に用いられることはありません。</br>
+                        </p>
+                        <?php
+                            echo $this->Form->control('user_detail.twitter_id',['type' => 'text']);
+                            echo $this->Form->control('email',['type' => 'text']);
+                        ?>
+                    </div>
                 </fieldset>
                 <?= $this->Form->button(__('変更'),['class' => 'btn btn-primary my-auto','id'=>'change-setting']) ?>
                 <?= $this->Form->end() ?>
@@ -30,7 +43,7 @@
         <div class="card-body text-dark pr-4 pl-4">
             <div class="password change form content">
                 <p>
-                    未実装
+                    現在のパスワードを入力すると、パスワードを変更可能です。
                 </p>
                 <?= $this->Form->create($user) ?>
                 <fieldset>
