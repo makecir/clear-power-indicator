e15dc48708cc184ca1d25265a88bfeb4172f9dac
makecir <konpeist@yahoo.co.jp>
2020-09-07 22:37:59 +0900

    Fix: グラフの部修正と注意書きの追加

--- templates/Scores/view.php
+++ templates/Scores/view.php
@@ -204,6 +204,19 @@
                                                 callback: function(value, index, values) {
                                                     return value + "%";
                                                 }
+                                            },
+                                            gridLines: {
+                                                color: ['rgba(0, 0, 0, 0.2)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.2)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                                'rgba(0, 0, 0, 0.1)',
+                                            ]
                                             }
                                         }]
                                     },
