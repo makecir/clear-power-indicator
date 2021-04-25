a0bc02381999aeceb8c6a58ee999237fc1e82cbd
makecir <konpeist@yahoo.co.jp>
2021-04-26 00:56:55 +0900

    Feat: 更新のツイート内容に順位を追加

--- templates/UserHistories/view.php
+++ templates/UserHistories/view.php
@@ -4,7 +4,7 @@
  * @var \App\Model\Entity\UserHistory $userHistory
  */
 
-$tweet_text = $userHistory->user->user_detail->dj_name.'さんがCPIを更新しました：'.$userHistory->rating_cur_info.' ('.$userHistory->rating_diff_info.")\n";
+$tweet_text = $userHistory->user->user_detail->dj_name.'さんがCPIを更新しました：CPI'.$userHistory->rating_cur_info.' ('.$userHistory->rating_diff_info.") ".$userHistory->standing_cur_info."位程度(".$userHistory->standing_diff_info.")\n";
 if($top_change['cpi']!=0)$tweet_text = $tweet_text."更新TOP：".($top_change['title']).' 【'.($lamp_info[$top_change['lamp']]).'】 (適正CPI'.$top_change['cpi'].")\n";
 
 ?>
