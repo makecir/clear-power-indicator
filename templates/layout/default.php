7144370ee9a9e23e74ab5c7bfd8a26fa4dea1921
makecir <konpeist@yahoo.co.jp>
2022-11-27 08:03:02 +0900

    Update: season 12 to 13

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -16,7 +16,7 @@
 use Cake\Routing\Router;
 
 $cakeDescription = 'CPI : Clear Power Indicator ';
-$source_version = "?ver=1.017";
+$source_version = "?ver=1.018";
 ?>
 <!DOCTYPE html>
 <html lang="ja">
